8c0934cbcd9719eccaa3ce8138296b793d370b4f
secelean <secelean@adobe.com>
2019-12-15 18:00:22 +0200

    add dummy ws endpoint

---
+++ application/test/controllers/AkkaWebSocketClient.scala
@@ -0,0 +1,47 @@
+package controllers
+
+import java.util.concurrent.LinkedBlockingDeque
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage, WebSocketRequest}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source, SourceQueueWithComplete}
+
+import scala.concurrent.Future
+
+class AkkaWebSocketClient {
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  type MessageQueue = LinkedBlockingDeque[String]
+  type SourceQueue = SourceQueueWithComplete[String]
+
+  private val messageQueue = new LinkedBlockingDeque[String]()
+
+  def connect(serverUrl: String): Future[(MessageQueue, SourceQueue)] = {
+    val incoming: Sink[Message, Future[Done]] = Sink.foreach {
+      case TextMessage.Strict(s) => messageQueue.offer(s)
+      case TextMessage.Streamed(s) => s.runFold("")(_ + _).foreach(messageQueue.offer)
+      case BinaryMessage.Strict(s) => messageQueue.offer(s.utf8String)
+      case BinaryMessage.Streamed(s) => s.runFold("")(_ + _.utf8String).foreach(messageQueue.offer)
+    }
+    val sourceQueue = Source.queue[String](Int.MaxValue, OverflowStrategy.backpressure)
+      .map { msg => TextMessage.Strict(msg) }
+    val (sourceMat, source) = sourceQueue.preMaterialize()
+    val flow: Flow[Message, Message, Future[Done]] = Flow.fromSinkAndSourceMat(incoming, source)(Keep.left)
+
+    val (upgradeResponse, closed) = Http().singleWebSocketRequest(WebSocketRequest(serverUrl), flow)
+    closed.foreach(_ => println("closed"))
+    upgradeResponse.map { upgrade =>
+      if (upgrade.response.status == StatusCodes.SwitchingProtocols) {
+        (messageQueue, sourceMat)
+      } else {
+        throw new RuntimeException(s"Connection failed: ${upgrade.response.status}")
+      }
+    }
+  }
+}
---
+++ application/test/controllers/WsFrontControllerSpec.scala
@@ -0,0 +1,25 @@
+package controllers
+
+
+
+import java.util.concurrent.TimeUnit
+
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.test._
+import play.api.test.Helpers._
+
+class WsFrontControllerSpec extends PlaySpec with GuiceOneServerPerSuite with Injecting {
+  val wsClient = new AkkaWebSocketClient
+
+  "WsFrontController" should {
+    "for a sent message an ack is received" in {
+      val serverUrl = s"ws://localhost:$port/ws"
+      val (in, out) = await(wsClient.connect(serverUrl))
+      await(out.offer("Hello"), 1000, TimeUnit.MILLISECONDS)
+
+      in.poll(1000, TimeUnit.MILLISECONDS) mustBe "Hello ack"
+
+    }
+  }
+}
